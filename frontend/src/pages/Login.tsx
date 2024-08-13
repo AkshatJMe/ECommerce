05effb30fe1ba7e709826202b6f4e6ab894afe14
Akshat Jain <realakshatjain@gmail.com>
2024-08-13 18:22:57 +0530

    redux done

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,36 +1,7 @@
 const Login = () => {
-  const dispatch = useDispatch();
-
   return (
-    <div className="login">
-      <main>
-        <h1 className="heading">Login</h1>
-
-        <div>
-          <label>Gender</label>
-          <select value={gender} onChange={(e) => setGender(e.target.value)}>
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-        </div>
-
-        <div>
-          <label>Date of birth</label>
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </div>
-
-        <div>
-          <p>Already Signed In Once</p>
-          <button onClick={loginHandler}>
-            <FcGoogle /> <span>Sign in with Google</span>
-          </button>
-        </div>
-      </main>
+    <div>
+      <h1>login</h1>
     </div>
   );
 };
